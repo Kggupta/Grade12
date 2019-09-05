1d59eee6c4f827826fef27b7f572074d595c6289
Kggupta <kgupt1@ocdsb.ca>
2019-09-05 11:06:23 -0400

    A1-Q14

---
+++ Assigment1/src/q14.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class q14 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter length of histogram: ");
+		int count = input.nextInt();
+		int[] numbers = new int[count];
+		while (true){
+			for(int i = 0;i < count;i++){
+				System.out.print("Enter number (-1 for exit): ");
+				int inputNumber = input.nextInt();
+				numbers[i] = inputNumber;
+			}
+			histogram(numbers);
+			return;
+		}
+	}
+
+	public static void histogram(int[] x){
+		for (int i = 0; i < x.length; i++) {
+			for (int t = 0;t<x[i];t++) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+}

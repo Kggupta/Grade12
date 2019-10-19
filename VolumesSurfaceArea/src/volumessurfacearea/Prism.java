e838310a39ed0c2416d6012480401efaabc55ae5
Kggupta <kgupt1@ocdsb.ca>
2019-10-19 17:08:44 -0400

    comments

--- VolumesSurfaceArea/src/volumessurfacearea/Prism.java
+++ VolumesSurfaceArea/src/volumessurfacearea/Prism.java
@@ -11,6 +11,8 @@ import java.util.InputMismatchException;
  *
  * @author keshavgupta
  */
+
+//Superclass Prism
 public class Prism {
     //All essential attributes
     double depth;
@@ -25,59 +27,61 @@ public class Prism {
         shapeName = "Prism";
     }
 
-    public double getSideLength(String prompt){
-        System.out.print(prompt);
-        Scanner input = new Scanner(System.in);
-        double sideLength;
-        while (true){
-            try{
-                sideLength = input.nextDouble();
-                break;
+    //Helper Method to get sideLengths
+    public double getSideLength(String prompt){//Takes in a prompt to specify what is being asked for
+        System.out.print(prompt);//Print prompt to console
+        Scanner input = new Scanner(System.in);//scanner as alias input
+        double sideLength;//variable to store the input
+        while (true){//continue asking for input until valid
+            try{//if sideLength can be casted to a double
+                sideLength = input.nextDouble();//assign input to variable
+                break;//stop loop
             }
-            catch (InputMismatchException err){
+            catch (InputMismatchException err){//if input was not a double
                 input.next();
-                System.out.print("Not a valid side length. " + prompt);
+                System.out.print("Not a valid side length. " + prompt);//reprompt the user for a valid sidelength
             }
         }
-        return sideLength;
+        return sideLength;//return the value of sidelength for assignment to whatever variable I specify
     }
 
-    public void assignSideLengths(){
-       while(true){
-            this.height = getSideLength("What is the height of the Prism? ");
-            if (this.height > 0) break;
-            else System.out.println("The Side Length cannot be less than 0.");
+    public void assignSideLengths(){//fill in the unknown sideLengths
+       while(true){//get height of prism
+            this.height = getSideLength("What is the height of the Prism? ");//assign returned sideLength to the object height attribute
+            if (this.height > 0) break;//if it is a valid side length (non negative value) end loop
+            else System.out.println("The Side Length cannot be less than 0.");//if it is an invalid sideLength, go back to getSideLength method
        }
-       while(true){
+       while(true){//see line 49
             this.width = getSideLength("What is the width of the Prism? ");
             if (this.width > 0) break;
             else System.out.println("The Side Length cannot be less than 0.");
        }
-       while(true){
+       while(true){//see line 49
             this.depth = getSideLength("What is the depth of the Prism? ");
             if (this.depth > 0) break;
             else System.out.println("The Side Length cannot be less than 0.");
        }
     }
 
-    public double calcVolume(){
+    public double calcVolume(){//method to calculate volume
         return this.depth * this.width * this.height;
     }
-    
-    public double calcSurfaceArea(){
+
+    public double calcSurfaceArea(){//method to calculate surface area
         return 2 * (this.depth * this.width + this.depth * this.height + this.width * this.height);
     }
-    
-    public void outputResults(){
+
+    public void outputResults(){//Output the calculated results
         System.out.format("The %s's volume is %s units cubed\nThe %s's surface area is %s units squared", this.shapeName,this.volume, this.shapeName, this.surfaceArea);
     }
-    
-    public static void main(String[] args) {
-        Prism shape1 = new Prism();
-        shape1.assignSideLengths();
-        shape1.volume = shape1.calcVolume();
-        shape1.surfaceArea = shape1.calcSurfaceArea();
-        shape1.outputResults();
+
+    public static void main(String[] args) {//Main code
+        //test shapes
+        Prism shape1 = new Prism();//New prism
+        shape1.assignSideLengths();//assign the prism it's sides
+        shape1.volume = shape1.calcVolume();//calculate prism volume based on lengths
+        shape1.surfaceArea = shape1.calcSurfaceArea();//calculate surface area based on lengths
+        shape1.outputResults();//output values to user
     }
 
 }

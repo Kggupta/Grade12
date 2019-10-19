cfd0e2e8bd3a8ead8a8fed1701c8ad89f2a04913
Kggupta <kgupt1@ocdsb.ca>
2019-10-19 16:29:23 -0400

    helper method for getting side lengths

--- VolumesSurfaceArea/src/volumessurfacearea/Prism.java
+++ VolumesSurfaceArea/src/volumessurfacearea/Prism.java
@@ -19,14 +19,31 @@ public class Prism {
     double surfaceArea;
     double volume;
     String shapeName;
-
+    
     //Prism constructor i want name of shape initialized as Prism
     public Prism(){
         shapeName = "Prism";
     }
 
-    public static void main(String[] args) {
+    public double getSideLength(String prompt){
+        System.out.print(prompt);
+        Scanner input = new Scanner(System.in);
+        double sideLength;
+        while (true){
+            try{
+                sideLength = input.nextDouble();
+                break;
+            }
+            catch (InputMismatchException err){
+                input.next();
+                System.out.print("Not a valid side length. " + prompt);
+            }
+        }
+        return sideLength;
+    }
 
+    public static void main(String[] args) {
+        Prism shape1 = new Prism();
 
     }
 

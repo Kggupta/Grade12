501d0dff17d0438e9eaf5f87fee3f17caa157b18
Kggupta <kgupt1@ocdsb.ca>
2019-10-19 18:16:32 -0400

    Sphere subclass

--- VolumesSurfaceArea/src/volumessurfacearea/Sphere.java
+++ VolumesSurfaceArea/src/volumessurfacearea/Sphere.java
@@ -10,5 +10,29 @@ package volumessurfacearea;
  * @author keshavgupta
  */
 public class Sphere extends Shape{
+    double radius;
+    
+    public Sphere(){
+        shapeName = "Sphere";
+    }
+    
+    @Override//override annotation
+    public void assignSideLengths(){
+       while(true){//get radius of sphere see shape.java for indepth comments
+            this.radius = getSideLength("What is the radius of the Sphere? ");
+            if (this.radius > 0) break;
+            else System.out.println("The radius cannot be less than 0.");
+       }
+    }
+    
+    @Override
+    public double calcVolume(){//method to calculate volume
+        return 4.18879 * Math.pow(this.radius,3);
+    }
+    
+    @Override
+    public double calcSurfaceArea(){//method to calculate surface area
+        return 4 * Math.PI * (this.radius * this.radius);
+    }
     
 }

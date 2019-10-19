cc8e286347f2f92e68b565a290858c0f7a5b64ef
Kggupta <kgupt1@ocdsb.ca>
2019-10-19 16:27:18 -0400

    prism attributes and constructor

---
+++ VolumesSurfaceArea/src/volumessurfacearea/Prism.java
@@ -0,0 +1,33 @@
+/*
+ * Name: Keshav
+ * Date: Sat Oct 19 2019
+ * Teacher: Ms.Luce
+ */
+package volumessurfacearea;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ *
+ * @author keshavgupta
+ */
+public class Prism {
+    //All essential attributes
+    double depth;
+    double width;
+    double height;
+    double surfaceArea;
+    double volume;
+    String shapeName;
+
+    //Prism constructor i want name of shape initialized as Prism
+    public Prism(){
+        shapeName = "Prism";
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
